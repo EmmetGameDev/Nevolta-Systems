47e9a4173859d931d5e31e3149af6d664e38377d
EmmetGameDev <emmetthegamedev@gmail.com>
2025-09-21 20:09:58 +0200

    Finished Specifications page

---
+++ src/js/specsScript.js
@@ -0,0 +1,27 @@
+// Navbar animate on scroll
+
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) { // adjust threshold as needed
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// Blog square photo Animations
+
+function showSquarePhoto(el){
+  el.classList.add("photoSquareAnim");
+}
+
+const photoSquareObsCallback = (entries) => {
+  for (const entry of entries) {
+    if (entry.isIntersecting){
+        setTimeout(() => showSquarePhoto(entry.target), 300);
+    }
+  }
+}
+
+const photoSquareObs = new IntersectionObserver(photoSquareObsCallback, { threshold: 0.25 });
+document.querySelectorAll('.blogSquarePhotoAnim').forEach(item => photoSquareObs.observe(item));
